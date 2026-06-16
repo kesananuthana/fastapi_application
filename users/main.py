0531ac90df459df28848bd3059974056fbb5dd0e
kesananuthana <kesananutana@gmail.com>
2026-06-16 10:24:18 +0530

    minor changes

--- users/main.py
+++ users/main.py
@@ -17,3 +17,7 @@ def get_db():
 def users(db:Session=Depends(get_db)):
     users = db.query(Employee).all()
     return {"data":users}
+
+@router.get("/hello")
+def user():
+    return {"message":"hello world"}
