b31b0c9f9674e468941d29f66af1dc94f6557957
kesananuthana <kesananutana@gmail.com>
2026-06-16 17:38:04 +0530

    middleware functionality is added

--- first.py
+++ first.py
@@ -10,7 +10,17 @@ from users.main import router as users_router
 from person.main import router as person_router
 from user_information.main import router as user_info_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(person_router)
